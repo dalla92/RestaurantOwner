3c7139f0dcc54a83443e67e571223b323704571a
Alessio <alex92_v@hotmail.it>
2016-04-06 09:33:18 +0200

    Restaurant_page first skeleton portrait layout sketch

--- app/src/main/java/it/polito/group2/restaurantowner/Restaurant_page.java
+++ app/src/main/java/it/polito/group2/restaurantowner/Restaurant_page.java
@@ -9,5 +9,8 @@ public class Restaurant_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_page);
+
+
+        //fill with mock info
     }
 }
