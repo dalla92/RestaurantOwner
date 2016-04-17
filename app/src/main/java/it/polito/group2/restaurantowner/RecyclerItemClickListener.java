795a147f9b00b333766508d0278ad9060b157521
Dalla92 <dani.dalla92@gmail.com>
2016-04-17 17:59:56 +0200

    added support for edit restaurant

--- app/src/main/java/it/polito/group2/restaurantowner/RecyclerItemClickListener.java
+++ app/src/main/java/it/polito/group2/restaurantowner/RecyclerItemClickListener.java
@@ -1,10 +1,12 @@
 package it.polito.group2.restaurantowner;
 
 import android.content.Context;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Switch;
 
 /**
  * Created by Daniele on 13/04/2016.
@@ -29,6 +31,17 @@ public class RecyclerItemClickListener implements RecyclerView.OnItemTouchListen
 
     @Override public boolean onInterceptTouchEvent(RecyclerView view, MotionEvent e) {
         View childView = view.findChildViewUnder(e.getX(), e.getY());
+        if(childView instanceof CardView) {
+            Switch sw = (Switch) childView.findViewById(R.id.meal_availability);
+            if(sw!=null) {
+                int[] values = new int[2];
+                int w = sw.getWidth();
+                int h = sw.getHeight();
+                childView.getLocationOnScreen(values);
+                if (e.getX() > values[0] && e.getX() < values[0] + w && e.getY() > values[1] && e.getY() < values[1] + h)
+                    return false;
+            }
+        }
         if (childView != null && mListener != null && mGestureDetector.onTouchEvent(e)) {
             mListener.onItemClick(childView, view.getChildPosition(childView));
             return true;
